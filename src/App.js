ba714f46c1d3f71bddf0e30956217972364af15b
AllefyNepomucena <allefy.s.nepomucena@gmail.com>
2022-02-06 20:21:15 -0300

    Aplicando Capas dos Filmes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React , {useEffect} from 'react'
+import React , {useEffect,useState} from 'react'
+import './App.css'
 import Tmdb from './Tmdb'
+import MovieRow from './componets/MovieRow'
 
 export default () => {
+
+  const [movieList , setMovieList] = useState([])
 
   useEffect(()=>{
     const loadAll = async () =>{
       //Pegando a Lista TOTAL 
      let list = await Tmdb.getHomelist()
-     console.log(list)
+    setMovieList(list)
     }
 
     loadAll()
@@ -15,6 +19,12 @@ export default () => {
   
   return (
     <div className='page'>
+    <section className='lists'>
+     {movieList.map((item, key)=>(
+     <MovieRow key={key} title={item.title} items={item.items}/>
+     ))}
+    </section>
+
       
     </div>
   )
