c8252047b15d9ae59c1ad9d5ffb30bcda3573697
AllefyNepomucena <allefy.s.nepomucena@gmail.com>
2022-04-02 14:02:20 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import FeaturedMovie from './componets/FeaturedMovie'
 export default () => {
 
   const [movieList , setMovieList] = useState([])
-  const [FeaturedData , setFeaturedData] = useState()
+  const [FeaturedData , setFeaturedData] = useState([])
 
   useEffect(()=>{
     const loadAll = async () =>{
@@ -20,7 +20,7 @@ export default () => {
     // Pegando o Featured
    
      let originals = list.filter(i=>i.slug === 'originals')
-     let randomChosen = Math.floor(Math.random() * (originals[0].items.results.leght - 1))
+     let randomChosen = Math.floor(Math.random() * (originals[0].items.results.lenght - 1))
      let chosen = originals[0].items.results[randomChosen]
 
     console.log(chosen)
