788f6a15456138be3aa8e56c38aec06f23158798
AllefyNepomucena <allefy.s.nepomucena@gmail.com>
2022-04-17 20:12:35 -0300

    Todas as Informações Pegadas

--- src/componets/FeaturedMovie.js
+++ src/componets/FeaturedMovie.js
@@ -3,6 +3,13 @@ import './FeaturedMovie.css'
 
 export default ({item}) => {
     console.log(item)
+
+    let firstDate = new Date(item.first_air_date)
+    let genres = []
+    for(let i in item.genres){
+        genres.push(item.genres [i].name)
+    }
+
     return(
         <section className="featured" style={
            {
@@ -16,14 +23,15 @@ export default ({item}) => {
                     <div className="featured--name">{item.original_name}</div>
                     <div className="featured--info">
                         <div className="featured--points">{item.vote_average} pontos</div>
-                        <div className="featured--year">2099</div>
+                        <div className="featured--year">{firstDate.getFullYear()}</div>
                         <div className="featured--seassons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1  ? 's' : ''}</div>
                     </div>
                     <div className="featured--description">{item.overview} </div>
                     <div className="featured--buttons">
-
+                        <a href={`watch/${item.id}`}>Assistir</a>
+                        <a href={`/list/add/${item.list}`}>+ Minha Lista</a>
                     </div>
-                    <div className="featured--genres"><strong>Gêneros:</strong>....</div>
+                    <div className="featured--genres"><strong>Gêneros:</strong>{genres.join(', ')}</div>
                 </div>
             </div>
         </section>
